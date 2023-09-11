cc7d26dd548c5ac2c72974196d57e46147267854
Ezra Varady <ezraavarady@gmail.com>
2023-09-10 15:28:52 -1000

    add gitignore and python loader for dataset

---
+++ load.py
@@ -0,0 +1,52 @@
+import psycopg2
+import os
+import time
+
+def process_cal256(root_dir):
+    subdirs_files_dict = {}
+    
+    for dirpath, dirnames, filenames in os.walk(root_dir):
+        if dirpath == root_dir:
+            continue
+            
+        subdir_name = os.path.basename(dirpath)
+        file_list = filenames
+        subdirs_files_dict[subdir_name] = file_list
+    
+    return subdirs_files_dict
+
+
+if __name__=='__main__':
+    files = process_cal256('256_ObjectCategories')
+    conn = psycopg2.connect(
+        host="localhost",
+        database="ubuntu",
+        user="ubuntu",
+        password="ubuntu",
+        port=5432
+    )
+
+    cur = conn.cursor()
+    cur.execute('''
+        CREATE TABLE IF NOT EXISTS image_table (
+            v REAL[],
+            location VARCHAR
+        );
+    ''')
+
+    conn.commit()
+
+    abs_start = time.time()
+    for i, k in enumerate(sorted(list(files.keys()))[198:]):
+        start = time.time()
+        for im in files[k]:
+            path = f'/home/ubuntu/256/{k}/{im}'
+            insert_query = f'INSERT INTO image_table (v, location) VALUES (clip_image(\'{path}\'), \'{path}\');'
+            cur.execute(insert_query)
+            conn.commit()
+        end = time.time()
+        print(f'completed {k} in {end-start} second, {end-abs_start} elapsed so far')
+
+    cur.execute('CREATE INDEX semantic_image ON image_table USING hnsw (v dist_cos_ops) WITH (M=5, ef=20, ef_construction=20);')
+    conn.commit()
+
